af2eec76fe8a6fe53c9f7d4d6b39cfda52fe57d7
venkata udaykiran p <venkataudaykiranp@h-PUday.ffe.foxeg.com>
2018-11-30 11:09:30 +0530

    Immutable String in Java

---
+++ src/main/java/com/java/string/immutable/example/StringImmutableExample.java
@@ -0,0 +1,37 @@
+package com.java.string.immutable.example;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 
+ * @author venkataudaykiranp
+ * 
+ * <p>
+ * 	<a href="https://www.javatpoint.com/immutable-string">Immutable String in Java</a>
+ * </p>
+ */
+@Slf4j
+public class StringImmutableExample {
+	
+	public static void main(String[] args) {
+		String s1 = "abc";
+		log.info("abc value: {}, hashcode: {}", s1, s1.hashCode());
+//		String s2 = "ABC";
+//		log.info("ABC value: {}, hashcode: {}", s2, s2.hashCode());
+		log.info("Trying to change abc value from {} to uppercase {} and hashcode {}", s1, s1.toUpperCase());
+		log.info("After update abc value: {}, hashcode: {}", s1, s1.hashCode());
+		
+		log.info("Trying to change abc value from {} to uppercase {} by assigning", s1, s1 = s1.toUpperCase());
+		log.info("After update abc value assigning: {}, hashcode: {}", s1, s1.hashCode());
+		
+		String name = "Mahendra Singh ";
+		name.concat("Dhoni");
+		log.info("Name: {}, hashcode: {}", name, name.hashCode());
+		String firstName = name;
+		log.info("FirstName: {}, hashcode: {}", firstName, firstName.hashCode());
+		name = name.concat("Dhoni");
+		log.info("Name: {}, hashcode: {}", name, name.hashCode());
+//		In such case, name points to the "Mahendra Singh Dhoni". Please notice that still "Mahendra Singh " object is not modified.
+		log.info("Name: {}, hashcode: {}, FirstName: {}, hashcode: {}", name, name.hashCode(), firstName, firstName.hashCode());
+	}
+}
