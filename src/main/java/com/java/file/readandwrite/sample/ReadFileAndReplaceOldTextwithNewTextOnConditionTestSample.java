9b8af331975cba5a236c60f7e154640870f0fd53
udaykiran pulipati <udaykiran pulipati@DESKTOP-4MF8HRC>
2019-03-16 09:52:17 +0530

    initial commit of sample and test

---
+++ src/main/java/com/java/file/readandwrite/sample/ReadFileAndReplaceOldTextwithNewTextOnConditionTestSample.java
@@ -0,0 +1,51 @@
+package com.java.file.readandwrite.sample;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class ReadFileAndReplaceOldTextwithNewTextOnConditionTestSample {
+
+	private String customer_name;
+	private Long title_list_id;
+	private String title_list_description;
+	private Long title_id;
+	private String title_name;
+	private Date license_start_date;
+	private Date license_end_date;
+	private Long window;
+	private Long total_fee;
+	private String assign_to;
+	public static void main(String[] args)
+	{
+		String path = "F:\\workspace\\EclipseOxygen\\JavaProject_v2\\src\\main\\java\\com\\java\\file\\readandwrite\\sample";
+		String fileName="ReadFileAndReplaceOldTextwithNewTextOnConditionTestSample.java";
+		File f=new File(path +"\\"+ fileName);
+		String pathName = f.getAbsolutePath();
+		System.out.println(pathName);
+		BufferedReader reader = null;
+		try {
+			reader = new BufferedReader(new FileReader(pathName));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		String line = "";
+		while(line != null)
+		{
+			try {
+				line = reader.readLine();
+				System.out.println(line);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		
+	}
+}
